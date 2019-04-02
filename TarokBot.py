e26241f35078a8e1a59a1758464cae3063e4ec45
Simon <oblaksss@gmail.com>
2019-04-02 19:26:22 +0200

    Shuffle cards

--- TarokBot.py
+++ TarokBot.py
@@ -9,27 +9,32 @@ def read_conf(file_name):
     properties = {}
     for line in lines:
         setting = line.split("=")
-        if len(setting) != 2:
-            print("CONFIGURATION SETTING not properly written")
-        else:
-            properties[setting[0]] = setting[1]
+        if len(setting) == 2:
+            try:
+                properties[setting[0]] = int(setting[1])
+            except ValueError:
+                properties[setting[0]] = setting[1]
+
     print("Configuration obtained")
     return properties
 
 
 conf = read_conf("resources/configuration.txt")
-print(conf)
-
+# print(conf)
 
 deck = Deck.Deck()
 
+deck.shuffle_deck(conf["min_cut_shuffle"],
+                  conf["max_cut_shuffle"],
+                  conf["min_number_of_shuffles"],
+                  conf["max_number_of_shuffles"])
+
+print("----------CARD ORDER----------")
 for kard in deck.deck:
-    print(kard.get_card_name())
+    print(kard.get_card_name() + "-> Points: " + kard.points + "; Rank: " + kard.rank)
+print("------------------------------")
 
 # Shuffle cards
 # Deal
 # Bot logic
-print("Test")
-
-
 
--- karte/Deck.py
+++ karte/Deck.py
@@ -1,4 +1,21 @@
 from karte import Card
+import random
+
+
+def cut_shuffle(deck, min_limit, max_limit):
+    spot = random.randint(min_limit, max_limit)
+    return deck[spot:] + deck[:spot]
+
+
+def riffle_shuffle(deck):
+    deck_half = int(len(deck) / 2)
+    first_half = deck[:deck_half]
+    second_half = deck[deck_half:]
+    result = []
+    for i in range(deck_half):
+        result.append(first_half[i])
+        result.append(second_half[i])
+    return result
 
 
 class Deck:
@@ -14,3 +31,11 @@ class Deck:
             card = Card.Card(name, points, rank, suit)
             deck.append(card)
         return deck
+
+    def shuffle_deck(self, min_limit, max_limit, min_number_of_shuffles, max_number_of_shuffles):
+        number_of_shuffles = random.randint(min_number_of_shuffles, max_number_of_shuffles)
+        for i in range(number_of_shuffles):
+            self.deck = cut_shuffle(self.deck, min_limit, max_limit)
+            self.deck = riffle_shuffle(self.deck)
+            i += 1
+
