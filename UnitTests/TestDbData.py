2d871ef7279bd1ec8f9fcb237c3782e6c113fa3a
Simon <oblaksss@gmail.com>
2020-04-21 01:13:09 +0200

    Database backup - RandomBot, WonderfulBot

--- UnitTests/TestDbData.py
+++ UnitTests/TestDbData.py
@@ -10,14 +10,12 @@ config = Configuration().get_config()
 class TestDbData(unittest.TestCase):
     def setUp(self):
         Db.connect_to_db()
-        self.min_id = 245
+        self.min_id = 425
 
     def test_if_54_rows_for_each_round_in_roundcards(self):
-        round_ids = Db.execute_select("SELECT round_id FROM rounds")
+        round_ids = Db.execute_select("SELECT round_id FROM rounds WHERE round_id > " + str(self.min_id))
         corrupted_ids = []
         for i in round_ids:
-            if i[0] < self.min_id:
-                continue
             results = Db.execute_select("SELECT count(*) FROM roundcards WHERE round_id = " + str(i[0]))
             print("Testing case for round_id: " + str(i[0]))
             if results[0][0] != 54:
